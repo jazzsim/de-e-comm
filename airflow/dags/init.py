c55b3494d4c7a673fa605043b1e5aed44f044be1
Jazz <jazz@info-in.net>
2025-02-27 21:08:26 +0800

    Update sql folder

--- airflow/dags/init.py
+++ airflow/dags/init.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 
 from pandas import DataFrame
+from airflow.utils.dates import days_ago
 from airflow.decorators import dag, task, task_group
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.trigger_rule import TriggerRule
@@ -12,7 +13,7 @@ logging.basicConfig(level=logging.INFO)
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime.datetime(2025, 2, 17),
+    'start_date': days_ago(1),
 }
 
 @dag(default_args=default_args, tags=['Initialization'], schedule="@once")
@@ -21,7 +22,7 @@ def initialize_warehouse_db():
     def purge():
         pg_hook = PostgresHook(postgres_conn_id='warehouse_db')
         try:
-            pg_hook.run("TRUNCATE TABLE dim_customer, dim_address, dim_category, dim_product, dim_customer_support, fact_sales, fact_payments, fact_order_details, fact_cart CASCADE", autocommit=True)
+            pg_hook.run("TRUNCATE TABLE dim_date, dim_customer, dim_address, dim_category, dim_product, dim_customer_support, fact_sales, fact_payments, fact_order_details, fact_cart CASCADE", autocommit=True)
 
         except Exception as e:
             logging.error(e)
@@ -94,7 +95,7 @@ def initialize_warehouse_db():
     def initDimDate():
         pg_hook = PostgresHook(postgres_conn_id='warehouse_db')
         
-        with open('/opt/airflow/dags/sql/dim_date.sql', 'r') as f:
+        with open('/opt/airflow/dags/sql/date.sql', 'r') as f:
             sql = f.read()
         pg_hook.run(sql, autocommit=True)
     
